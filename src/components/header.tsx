bb4663f68212c02f7d1a5d1aa24dfbd2a2fdecd9
Svetlana Zubareva <se.berseneva@gmail.com>
2024-12-22 19:51:43 +0300

    3.3. Обновит ссылку для логотипа.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,6 @@
+import Logo from "./list-item-card/logo";
+
+
 type Header = {
   apartCount: number;
   email: string;
@@ -8,9 +11,7 @@ function HeaderScreen({apartCount, email}: Header): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </a>
+              <Logo/>
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,9 @@
+import Logo from "../components/list-item-card/logo";
+
 function Mistake(): JSX.Element{
   return(
     <section>
-        <a className="header__logo-link header__logo-link--active">
-          <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-        </a>
+        <Logo/>
         <h2>
           404 Not Found
         </h2>
