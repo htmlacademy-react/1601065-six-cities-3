eeb844a4773fd477c36d21b65b464ee2597c58b9
Svetlana Zubareva <se.berseneva@gmail.com>
2024-12-22 20:36:14 +0300

    3.5. Заменит Route на PrivateRoute.

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,10 +1,11 @@
-import {Route, BrowserRouter, Routes} from 'react-router-dom';
-import {AppRoute} from '../const/const.ts';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../const/const.ts';
 import MainScreen from '../pages/main-pages.tsx';
 import FavoriteScreen from '../pages/favorite-page.tsx';
 import LoginScreen from '../pages/login-page.tsx';
 import OfferScreen from '../pages/offer-page.tsx';
 import Mistake from '../pages/404.tsx';
+import PrivateRoute from './private-route.tsx';
 
 
 function App(): JSX.Element {
@@ -21,7 +22,13 @@ function App(): JSX.Element {
       />
       <Route
         path={AppRoute.Favorite}
-        element={<FavoriteScreen />}
+        element={
+          <PrivateRoute
+            authorizationStatus={AuthorizationStatus.NoAuth}
+          >
+            <FavoriteScreen />
+          </PrivateRoute>
+        }
       />
       <Route
         path={AppRoute.Offer}
