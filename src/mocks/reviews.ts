0884cfe39a5e8ffe1bfdf3b15d1744e285697cf9
Svetlana Zubareva <se.berseneva@gmail.com>
2025-01-29 17:39:44 +0300

    моки для задания 4.21. Обратная сторона реальности

---
+++ src/mocks/reviews.ts
@@ -0,0 +1,46 @@
+export const reviews = [
+  {
+    id: '1',
+    offerId: '1',
+    user: {
+      name: 'Алексей Иванов',
+      avatarUrl: 'https://example.com/avatar1.jpg',
+    },
+    rating: 5,
+    comment: 'Отличная квартира, чистая и уютная! Хозяин очень доброжелательный.',
+    date: '2025-01-15',
+  },
+  {
+    id: '2',
+    offerId: '2',
+    user: {
+      name: 'Марина Смирнова',
+      avatarUrl: 'https://example.com/avatar2.jpg',
+    },
+    rating: 4,
+    comment: 'Апартаменты шикарные, но было шумно по ночам.',
+    date: '2025-01-18',
+  },
+  {
+    id: '3',
+    offerId: '3',
+    user: {
+      name: 'Игорь Петров',
+      avatarUrl: 'https://example.com/avatar3.jpg',
+    },
+    rating: 5,
+    comment: 'Идеальное место для отдыха, пляж рядом, всё супер!',
+    date: '2025-01-20',
+  },
+  {
+    id: '4',
+    offerId: '4',
+    user: {
+      name: 'Елена Кузнецова',
+      avatarUrl: 'https://example.com/avatar4.jpg',
+    },
+    rating: 3,
+    comment: 'Лофт стильный, но не хватало удобств для длительного проживания.',
+    date: '2025-01-22',
+  },
+];
