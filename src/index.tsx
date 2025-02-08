f0bf955c2639f7f2c79cf01322a39fc084fd4ca3
Svetlana Zubareva <se.berseneva@gmail.com>
2025-02-08 13:33:53 +0300

    build

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app.tsx';
-import { Setting } from './const/const.ts';
-import { offers } from './mocks/offers.ts';
-import { reviews } from './mocks/reviews.ts';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -11,11 +8,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App
-      apartCount={Setting.apartCount}
-      email={Setting.email}
-      offers={offers}
-      reviews={reviews}
-    />
+    <App />
   </React.StrictMode>,
 );
