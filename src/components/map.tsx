d6776a1778a86ff020b5064b269fff8836d5b8bd
Zubareva Svetlana <zubareva.s@roistat.com>
2025-03-23 22:27:49 +0300

    исправила ошибки линтера

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import { AMSTERDAM_COORDS } from '../const/const.ts'
+import { AMSTERDAM_COORDS } from '../const/const.ts';
 import { OfferMap } from '../types/type.ts';
 
 type MapProps = {
@@ -9,17 +9,14 @@ type MapProps = {
 };
 
 const Map = ({ offers }: MapProps) => {
-
   useEffect(() => {
-
     const map = L.map('map').setView(AMSTERDAM_COORDS, 12);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
     }).addTo(map);
 
-
-    offers.forEach(offer => {
+    offers.forEach((offer) => {
       const marker = L.marker([offer.location.latitude, offer.location.longitude]).addTo(map);
       marker.bindPopup(offer.title);
     });
@@ -29,12 +26,7 @@ const Map = ({ offers }: MapProps) => {
     };
   }, [offers]);
 
-  return (
-    <div
-      id="map"
-      style={{ height: '100%', width: '100%' }}
-    />
-  );
+  return <div id="map" style={{ height: '100%', width: '100%' }} />;
 };
 
 export default Map;
